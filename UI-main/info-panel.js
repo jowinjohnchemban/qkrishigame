3868868c3ba350fcdb34987183aa56fb370034bb
rupa <61702875+rupa1729@users.noreply.github.com>
2022-10-15 18:57:36 +0530

    Add files via upload

---
+++ UI-main/info-panel.js
@@ -0,0 +1,66 @@
+/* global AFRAME */
+AFRAME.registerComponent('info-panel', {
+  init: function () {
+    var buttonEls = document.querySelectorAll('.menu-button');
+    var fadeBackgroundEl = this.fadeBackgroundEl = document.querySelector('#fadeBackground');
+
+    this.movieImageEl;
+    this.movieTitleEl = document.querySelector('#movieTitle');
+    this.movieDescriptionEl = document.querySelector('#movieDescription');
+
+    this.movieInfo = {
+     X : {
+        title: 'X-gate',
+        imgEl: document.querySelector('#X'),
+        description:'X-gate switches the amplitudes of states |0> and |1>.'
+      },
+      Z : {
+        title: 'Z-gate',
+        imgEl: document.querySelector('#Z'),
+        description: 'Z-gate have no effect on qubit when it is |0> and |1> states because they are eigen states of Z-gate '
+      },
+      H : {
+        title: 'H-gate',
+        imgEl: document.querySelector('#H'),
+        description: 'H-gate helps quantum state to be in superposition'
+      }
+    };
+
+    this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
+    this.onBackgroundClick = this.onBackgroundClick.bind(this);
+    this.backgroundEl = document.querySelector('#background');
+    for (var i = 0; i < buttonEls.length; ++i) {
+      buttonEls[i].addEventListener('click', this.onMenuButtonClick);
+    }
+    this.backgroundEl.addEventListener('click', this.onBackgroundClick);
+    this.el.object3D.renderOrder = 9999999;
+    this.el.object3D.depthTest = false;
+    fadeBackgroundEl.object3D.renderOrder = 9;
+    fadeBackgroundEl.getObject3D('mesh').material.depthTest = false;
+  },
+
+  onMenuButtonClick: function (evt) {
+    var movieInfo = this.movieInfo[evt.currentTarget.id];
+
+    this.backgroundEl.object3D.scale.set(1, 1, 1);
+
+    this.el.object3D.scale.set(1, 1, 1);
+    if (AFRAME.utils.device.isMobile()) { this.el.object3D.scale.set(1.4, 1.4, 1.4); }
+    this.el.object3D.visible = true;
+    this.fadeBackgroundEl.object3D.visible = true;
+
+    if (this.movieImageEl) { this.movieImageEl.object3D.visible = false; }
+    this.movieImageEl = movieInfo.imgEl;
+    this.movieImageEl.object3D.visible = true;
+
+    this.movieTitleEl.setAttribute('text', 'value', movieInfo.title);
+    this.movieDescriptionEl.setAttribute('text', 'value', movieInfo.description);
+  },
+
+  onBackgroundClick: function (evt) {
+    this.backgroundEl.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.visible = false;
+    this.fadeBackgroundEl.object3D.visible = false;
+  }
+});
